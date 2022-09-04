334f36d2b9acc90e905db4bd78e9d3ba1fb41b51
macmini-m1-2 <jaisonjoseph@macmini-m1-2s-Mac-mini.local>
2022-09-04 21:30:51 +0530

    First Commit

---
+++ context-app/src/components/BookList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function BookList() {
+  return (
+    <div className='book-list'>
+      <ul>
+        <li>
+            Book 1
+        </li>
+        <li>
+            Book 2
+        </li>
+      </ul>
+    </div>
+  )
+}
---
+++ context-app/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='home'>
+      <ul>
+        <li>
+            Home
+        </li>
+        <li>
+            About
+        </li>
+        <li>
+            Contact
+        </li>
+      </ul>
+    </div>
+  )
+}
